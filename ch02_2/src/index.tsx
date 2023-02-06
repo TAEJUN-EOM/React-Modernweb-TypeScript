f944166da5efdff4b4d53766bc46d76b9484ff0d
Psychopomp <91600307+TAEJUN-EOM@users.noreply.github.com>
2023-02-06 21:26:33 +0900

    ch02

---
+++ ch02_2/src/index.tsx
@@ -0,0 +1,56 @@
+//import React from 'react'
+import ReactDOM from 'react-dom/client'
+import * as D from './data'
+
+/*const CE = React.createElement
+
+const rootVirtualDOM = CE('ul', null, [
+  CE('li', null, [
+    CE('a', {href: 'http://www.google.com', target: '_blank'}, [
+      CE('p', null, 'go to google')
+    ])
+  ])
+])*/
+
+/*
+const rootVirtualDOM = (
+  <ul>
+    <li>
+      <a href="http://www.google.com" target="_blank">
+        <p>go to Google</p>
+      </a>
+    </li>
+  </ul>
+)*/
+
+/*const children = [
+  <li>
+    <a href="http://www.google.com" target="_blank">
+      <p>go to Google</p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://www.facebook.com" target="_blank">
+      <p>go to Facebook</p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://www.twitter.com" target="_blank">
+      <p>go to Twitter</p>
+    </a>
+  </li>
+]
+const rootVirtualDOM = <ul>{children}</ul>*/
+
+const children = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src={D.randomAvatar()} width={100} height={100} />
+  </div>
+))
+const rootVirtualDOM = <div>{children}</div>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(rootVirtualDOM)
